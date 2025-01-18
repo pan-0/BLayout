8f1aa90dcb4472a727f393462f6fc1e9a70e17d4
pan <>
2025-01-18 02:20:06 +0200

    header: sacrifice readability

--- blayout.h
+++ blayout.h
@@ -33,58 +33,73 @@
  */
 
 #ifndef BL_ASSERT
-#	include <assert.h>  /* assert() */
-#	define BL_ASSERT assert
+#include <assert.h>  /* assert() */
+#define BL_ASSERT assert
 #endif
 
 #ifndef BL_ALIGNMENT
-#	if defined __cplusplus
-#		include <cstddef>  /* std::max_align_t */
-#		define BL_ALIGNMENT alignof(std::max_align_t)
-#	elif !defined _MSC_VER && defined __STDC_VERSION__ && \
-			__STDC_VERSION__ >= 201112L  /* C11 */
-#		define BL_ALIGNMENT _Alignof(max_align_t)
-#	else
-		/*
-		 * MSVC doesn't have `max_align_t`.
-		 *
-		 * <https://learn.microsoft.com/en-ie/answers/questions/1726147/why-max-align-t-not-defined-in-stddef-h-in-windows>
-		 * <https://developercommunity.visualstudio.com/t/stdc11-should-add-max-align-t-to-stddefh/1386891>
-		 * <https://github.com/emscripten-core/emscripten/pull/8908>
-		 */
-		union bl_max_align {
-			long long ll;
-			long double ld;
-		};
-#		if defined __STDC_VERSION__ && __STDC_VERSION__ < 201112L
-#			ifdef __GNUC__
-				/* <https://gcc.gnu.org/onlinedocs/gcc-2.95.3/gcc_4.html#SEC89> */
-#				define BL_ALIGNMENT __alignof__(union bl_max_align)
-#			else
-				/* C99 and earlier standards don't support `_Alignof`. */
-				struct bl_max_align99 {
-					char c;
-					union bl_max_align a;
-				};
-#				define BL_ALIGNMENT offsetof(struct bl_max_align99, a)
-#			endif
-#		else
-#			define BL_ALIGNMENT _Alignof(union bl_max_align)
-#		endif
-#	endif
+/* C++ */
+#if defined __cplusplus
+#if __cplusplus >= 201103L  /* C++11 */
+#include <cstddef>  /* std::max_align_t */
+#define BL_ALIGNMENT alignof(std::max_align_t)
+/* else :shrug-face: */
+#endif
+/* !MSVC && >=C11 */
+#elif !defined _MSC_VER && defined __STDC_VERSION__ && \
+__STDC_VERSION__ >= 201112L  /* C11 */
+#if __STDC_VERSION__ >= 202311L  /* C23 */
+#define BL_ALIGNMENT alignof(max_align_t)
+#else
+#define BL_ALIGNMENT _Alignof(max_align_t)
+#endif
+/*
+ * MSVC doesn't have `max_align_t`.
+ *
+ * <https://learn.microsoft.com/en-ie/answers/questions/1726147/why-max-align-t-not-defined-in-stddef-h-in-windows>
+ * <https://developercommunity.visualstudio.com/t/stdc11-should-add-max-align-t-to-stddefh/1386891>
+ * <https://github.com/emscripten-core/emscripten/pull/8908>
+ */
+#else
+union bl_max_align {
+	long long _ll;
+	long double _ld;
+};
+#if defined __STDC_VERSION__ && __STDC_VERSION__ >= 202311L  /* C23 */
+#define BL_ALIGNMENT alignof(union bl_max_align)
+#elif defined __STDC_VERSION__ && __STDC_VERSION__ >= 201112L  /* C11 */
+#define BL_ALIGNMENT _Alignof(union bl_max_align)
+#elif defined _MSC_VER
+/* <https://learn.microsoft.com/en-us/cpp/cpp/alignment-cpp-declarations?view=msvc-170> */
+#define BL_ALIGNMENT __alignof(union bl_max_align)
+#elif defined __GNUC__
+/* <https://gcc.gnu.org/onlinedocs/gcc-2.95.3/gcc_4.html#SEC89> */
+#define BL_ALIGNMENT __alignof__(union bl_max_align)
+#else
+/* C99 and earlier standards don't support `_Alignof`. */
+struct bl_max_align99 {
+	char _c;
+	union bl_max_align _a;
+};
+#define BL_ALIGNMENT offsetof(struct bl_max_align99, _a)
+#endif
+#endif
 #endif
 
 #ifndef BL_API
-#	define BL_API static
+#define BL_API static
 #endif
 
+#ifndef BL_INLINE
+#define BL_INLINE inline
+#endif
 
 /*
  * Types.
  */
 
-typedef size_t blsize;  /* Can also be a `signed` type. */
-#define BL_SIZE_MAX SIZE_MAX
+typedef size_t blsize;       /* Can also be a `signed` type. */
+#define BL_SIZEMAX SIZE_MAX  /* Must be `> 0`. */
 
 struct blayout {
 	blsize nmemb;
@@ -98,105 +113,107 @@ struct blayout {
  */
 
 #if defined __GNUC__
-__attribute__((__always_inline__, __pure__)) inline
+__attribute__((__always_inline__, __pure__)) BL_INLINE
 #elif defined _MSC_VER
 __forceinline
 #else
-inline
+BL_INLINE
 #endif
-BL_API blsize blcalc(blsize align,
-                     ptrdiff_t offs,
-                     blsize n,
-                     const struct blayout *lays,
-                     blsize prev_size)
+BL_API blsize blcalc(blsize _align,
+                     ptrdiff_t _offs,
+                     blsize _n,
+                     const struct blayout *_lays,
+                     blsize _prev_size)
 {
-	size_t base = (size_t)align + (size_t)offs;
-	size_t pos = base;
-	blsize i;
+	size_t _base = (size_t)_align + (size_t)_offs;
+	size_t _pos = _base;
+	blsize _i;
 
-	BL_ASSERT(align > 0 && ((size_t)align & ((size_t)align - 1)) == 0 &&
-	          offs >= 0 && n > 0 && lays != (void *)0 &&
-	          /* prev_size >= 0 && */ base >= (size_t)align);
+	BL_ASSERT(_align > 0 && ((size_t)_align & ((size_t)_align - 1)) == 0 &&
+	          _offs >= 0 && _n > 0 && _lays != (void *)0 &&
+	          /* _prev_size >= 0 && */ _base >= (size_t)_align);
 
-	if (pos + (size_t)prev_size < pos)
+	if (_pos + (size_t)_prev_size < _pos)
 		return 0;
 
-	pos += (size_t)prev_size;
-	for (i = 0; i < n; ++i) {
-		size_t size;
-		size_t pad;
-		struct blayout l = lays[i];
-		BL_ASSERT(l.nmemb > 0 && l.size > 0 && l.align > 0 &&
-		          ((size_t)l.align & ((size_t)l.align - 1)) == 0);
-		if (l.nmemb > BL_SIZE_MAX / l.size)
+	_pos += (size_t)_prev_size;
+	for (_i = 0; _i < _n; ++_i) {
+		size_t _size;
+		size_t _pad;
+		struct blayout _l = _lays[_i];
+		BL_ASSERT(_l.nmemb > 0 && _l.size > 0 && _l.align > 0 &&
+		          ((size_t)_l.align & ((size_t)_l.align - 1)) == 0);
+		if (_l.nmemb > BL_SIZEMAX / _l.size)
 			return 0;
 
-		size = (size_t)l.nmemb * (size_t)l.size;
-		pad = ~(pos - 1) & ((size_t)l.align - 1);
-		if (size + pad < size)
+		_size = (size_t)_l.nmemb * (size_t)_l.size;
+		_pad = ~(_pos - 1) & ((size_t)_l.align - 1);
+		if (_size + _pad < _size)
 			return 0;
 
-		size += pad;
-		if (pos + size < pos)
+		_size += _pad;
+		if (_pos + _size < _pos)
 			return 0;
 
-		pos += size;
+		_pos += _size;
 	}
 
-	return (blsize)(pos - base);
+	_pos -= _base;
+	return _pos > BL_SIZEMAX ? 0 : (blsize)_pos;
 }
 
 #ifdef __GNUC__
 __attribute__((__returns_nonnull__, __nonnull__(1), __alloc_align__(3)))
 #endif
-inline
-BL_API void *blnext(void *ptr, blsize curr_size, blsize next_align)
+BL_INLINE
+BL_API void *blnext(void *_ptr, blsize _curr_size, blsize _next_align)
 {
-	size_t pad;
-#	ifndef __clang__
-		BL_ASSERT(ptr != (void *)0);
-#	endif
-	BL_ASSERT(/* curr_size >= 0 && */ next_align > 0 &&
-	          ((size_t)next_align & ((size_t)next_align - 1)) == 0);
-	ptr = (char *)ptr + curr_size;
-	pad = (size_t)(~((uintptr_t)ptr - 1) & ((size_t)next_align - 1));
-	return (char *)ptr + pad;
+	size_t _pad;
+#ifndef __clang__
+	BL_ASSERT(_ptr != (void *)0);
+#endif
+	BL_ASSERT(/* _curr_size >= 0 && */ _next_align > 0 &&
+	          ((size_t)_next_align & ((size_t)_next_align - 1)) == 0);
+	_ptr = (char *)_ptr + _curr_size;
+	_pad = (size_t)(~((uintptr_t)_ptr - 1) & ((size_t)_next_align - 1));
+	return (char *)_ptr + _pad;
 }
 
 #ifdef __GNUC__
 __attribute__((__returns_nonnull__, __nonnull__(1), __alloc_size__(2),
                __alloc_align__(3)))
 #endif
-inline
-BL_API void *blprev(void *ptr, blsize prev_size, blsize prev_align)
+BL_INLINE
+BL_API void *blprev(void *_ptr, blsize _prev_size, blsize _prev_align)
 {
-#	ifndef __clang__
-		BL_ASSERT(ptr != (void *)0);
-#	endif
-	BL_ASSERT(prev_size > 0 && prev_align > 0 &&
-	          ((size_t)prev_align & ((size_t)prev_align - 1)) == 0);
-	ptr = (char *)ptr - prev_size;
-	ptr = (char *)ptr - (size_t)((uintptr_t)ptr & ((size_t)prev_align - 1));
-	return ptr;
+	size_t _pad;
+#ifndef __clang__
+	BL_ASSERT(_ptr != (void *)0);
+#endif
+	BL_ASSERT(_prev_size > 0 && _prev_align > 0 &&
+	          ((size_t)_prev_align & ((size_t)_prev_align - 1)) == 0);
+	_ptr = (char *)_ptr - _prev_size;
+	_pad = (size_t)((uintptr_t)_ptr & ((size_t)_prev_align - 1));
+	return (char *)_ptr - _pad;
 }
 
 #ifdef __GNUC__
 __attribute__((__always_inline__, __nonnull__(1), __pure__))
 #endif
-inline
-BL_API blsize blsizeof(const struct blayout *l)
+BL_INLINE
+BL_API blsize blsizeof(const struct blayout *_l)
 {
-	blsize nmemb;
-	blsize size;
-#	ifndef __clang__
-		BL_ASSERT(l != (void *)0);
-#	endif
-	nmemb = l->nmemb;
-	size = l->size;
-	BL_ASSERT(nmemb > 0 && size > 0 && l->align > 0 &&
-	          ((size_t)l->align & ((size_t)l->align - 1)) == 0 &&
-	          !(nmemb > BL_SIZE_MAX / size));
-	return nmemb * size;
+	blsize _nmemb;
+	blsize _size;
+#ifndef __clang__
+	BL_ASSERT(_l != (void *)0);
+#endif
+	_nmemb = _l->nmemb;
+	_size = _l->size;
+	BL_ASSERT(_nmemb > 0 && _size > 0 && _l->align > 0 &&
+	          ((size_t)_l->align & ((size_t)_l->align - 1)) == 0 &&
+	          !(_nmemb > BL_SIZEMAX / _size));
+	return _nmemb * _size;
 }
 
 #endif  /* BLAYOUT_H */
