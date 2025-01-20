758aa3d4ace0430aa138fec5e8afca88bd53fde3
pan <>
2025-01-20 21:27:27 +0200

    header: various improvements

--- blayout.h
+++ blayout.h
@@ -29,12 +29,40 @@
 
 
 /*
+ * Uncomment any of these if you want to override the default values.
+ */
+
+/*#define BL_SIZEMAX   SIZE_MAX*/
+/*#define BL_ALIGNMENT alignof(max_align_t)*/
+/*#define BL_API       static*/
+/*#define BL_ASSERT    assert*/
+/*#define BL_INLINE    inline*/
+/*#define BL_DEBUG     0*/
+
+
+/*
  * Boilerplate.
  */
 
+#if !defined BL_DEBUG || BL_DEBUG == 0
+#define BL_ATTR(attr) __attribute__((attr))
+#if 0 && !defined BL_ASSERT
+#define BL_ASSERT(_)
+#endif
+#elif BL_DEBUG == 1
+#define BL_ATTR(attr) __attribute__((attr))
 #ifndef BL_ASSERT
 #include <assert.h>  /* assert() */
 #define BL_ASSERT assert
+#endif
+#elif BL_DEBUG == 2
+#define BL_ATTR(_)
+#ifndef BL_ASSERT
+#include <assert.h>  /* assert() */
+#define BL_ASSERT assert
+#endif
+#else
+#error "invalid `BL_DEBUG` value, must be `0`, `1`, or `2`"
 #endif
 
 #ifndef BL_ALIGNMENT
@@ -91,6 +119,9 @@ struct bl_max_align99 {
 
 #ifndef BL_INLINE
 #define BL_INLINE inline
+#define BL_INLINE_USER 0
+#else
+#define BL_INLINE_USER 1
 #endif
 
 
@@ -99,7 +130,9 @@ struct bl_max_align99 {
  */
 
 typedef size_t blsize;       /* Can also be a `signed` type. */
+#ifndef BL_SIZEMAX
 #define BL_SIZEMAX SIZE_MAX  /* Must be `> 0`. */
+#endif
 
 struct blayout {
 	blsize nmemb;
@@ -113,8 +146,12 @@ struct blayout {
  */
 
 #if defined __GNUC__
-__attribute__((__always_inline__, __pure__)) BL_INLINE
-#elif defined _MSC_VER
+__attribute__((__pure__))
+#if !BL_INLINE_USER
+BL_ATTR(__always_inline__)
+#endif
+BL_INLINE
+#elif !BL_INLINE_USER && defined _MSC_VER
 __forceinline
 #else
 BL_INLINE
@@ -129,9 +166,11 @@ BL_API blsize blcalc(blsize _align,
 	size_t _pos = _base;
 	blsize _i;
 
+#if defined BL_DEBUG && BL_DEBUG >= 1
 	BL_ASSERT(_align > 0 && ((size_t)_align & ((size_t)_align - 1)) == 0 &&
 	          _offs >= 0 && _n > 0 && _lays != (void *)0 &&
 	          /* _prev_size >= 0 && */ _base >= (size_t)_align);
+#endif
 
 	if (_pos + (size_t)_prev_size < _pos)
 		return 0;
@@ -141,8 +180,10 @@ BL_API blsize blcalc(blsize _align,
 		size_t _size;
 		size_t _pad;
 		struct blayout _l = _lays[_i];
+#if defined BL_DEBUG && BL_DEBUG >= 1
 		BL_ASSERT(_l.nmemb > 0 && _l.size > 0 && _l.align > 0 &&
 		          ((size_t)_l.align & ((size_t)_l.align - 1)) == 0);
+#endif
 		if (_l.nmemb > BL_SIZEMAX / _l.size)
 			return 0;
 
@@ -163,57 +204,77 @@ BL_API blsize blcalc(blsize _align,
 }
 
 #ifdef __GNUC__
-__attribute__((__returns_nonnull__, __nonnull__(1), __alloc_align__(3)))
+BL_ATTR(__returns_nonnull__) BL_ATTR(__nonnull__(1))
+__attribute__((__alloc_align__(3)))
 #endif
 BL_INLINE
 BL_API void *blnext(void *_ptr, blsize _curr_size, blsize _next_align)
 {
 	size_t _pad;
-#ifndef __clang__
+#ifdef BL_DEBUG
+#if BL_DEBUG >= 2
 	BL_ASSERT(_ptr != (void *)0);
 #endif
+#if BL_DEBUG >= 1
 	BL_ASSERT(/* _curr_size >= 0 && */ _next_align > 0 &&
 	          ((size_t)_next_align & ((size_t)_next_align - 1)) == 0);
+#endif
+#endif
 	_ptr = (char *)_ptr + _curr_size;
 	_pad = (size_t)(~((uintptr_t)_ptr - 1) & ((size_t)_next_align - 1));
 	return (char *)_ptr + _pad;
 }
 
 #ifdef __GNUC__
-__attribute__((__returns_nonnull__, __nonnull__(1), __alloc_size__(2),
-               __alloc_align__(3)))
+BL_ATTR(__returns_nonnull__) BL_ATTR(__nonnull__(1))
+__attribute__((__alloc_size__(2), __alloc_align__(3)))
 #endif
 BL_INLINE
 BL_API void *blprev(void *_ptr, blsize _prev_size, blsize _prev_align)
 {
 	size_t _pad;
-#ifndef __clang__
+#ifdef BL_DEBUG
+#if BL_DEBUG >= 2
 	BL_ASSERT(_ptr != (void *)0);
 #endif
+#if BL_DEBUG >= 1
 	BL_ASSERT(_prev_size > 0 && _prev_align > 0 &&
 	          ((size_t)_prev_align & ((size_t)_prev_align - 1)) == 0);
+#endif
+#endif
 	_ptr = (char *)_ptr - _prev_size;
 	_pad = (size_t)((uintptr_t)_ptr & ((size_t)_prev_align - 1));
 	return (char *)_ptr - _pad;
 }
 
 #ifdef __GNUC__
-__attribute__((__always_inline__, __nonnull__(1), __pure__))
+BL_ATTR(__nonnull__(1)) __attribute__((__pure__))
+#if !BL_INLINE_USER
+BL_ATTR(__always_inline__)
+#endif
 #endif
 BL_INLINE
 BL_API blsize blsizeof(const struct blayout *_l)
 {
 	blsize _nmemb;
 	blsize _size;
-#ifndef __clang__
+#if defined BL_DEBUG && BL_DEBUG >= 2
 	BL_ASSERT(_l != (void *)0);
 #endif
 	_nmemb = _l->nmemb;
 	_size = _l->size;
+#if defined BL_DEBUG && BL_DEBUG >= 1
 	BL_ASSERT(_nmemb > 0 && _size > 0 && _l->align > 0 &&
 	          ((size_t)_l->align & ((size_t)_l->align - 1)) == 0 &&
 	          !(_nmemb > BL_SIZEMAX / _size));
+#endif
 	return _nmemb * _size;
 }
+
+#if !BL_INLINE_USER
+#undef BL_INLINE
+#endif
+#undef BL_INLINE_USER
+#undef BL_ATTR
 
 #endif  /* BLAYOUT_H */
